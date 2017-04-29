a3eec3b9801349da9b99a837224a9c0e39855872
Robby Schlesinger <robbyschlesinger@Robbys-MacBook-Pro.local>
2017-04-29 13:19:52 -0700

    map.js

--- app/public-src/app.js
+++ app/public-src/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Map from './components/map/map';
 
 export default class App extends Component {
 	render() {
 		return (
-			<div>Hello, World!</div>
+			<div><Map zoom={10} center={{ lat: 39.5252966, lng: -119.8166583 }}/></div>
 		);
 	}
 }
