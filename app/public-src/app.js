7b4498a7205ac4b774c5c87b5b0c360d8ec49258
Robby Schlesinger <robbyschlesinger@Robbys-MacBook-Pro.local>
2017-04-29 17:05:54 -0700

    changes

--- app/public-src/app.js
+++ app/public-src/app.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import Map from './components/map/map';
 import Player from './components/player/player';
 import Search from './components/search/search';
-import Icon from 'app-components/icon/icon';
+import Icons from './components/icons/icons';
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<Search />
-				<Icon style={{ color: 'white', zIndex: 3, fontSize: '4rem', position: 'absolute' }} name='compass'/>
 				<Map zoom={10} center={{ lat: 39.5252966, lng: -119.8166583 }} />
+				<Icons />
 				<Player handleChange={ (year) => {
 					console.log(year);
 				}}/>
